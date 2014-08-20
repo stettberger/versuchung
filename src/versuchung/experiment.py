4e1e3db9147c6d955a5bd4e80c6aaf583b93a499
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2014-08-20 15:47:07 +0200

    Add copyright notices
    Change-Id: I38aa1b7cb792c52fc286bc3426e88bd126df655e

--- src/versuchung/experiment.py
+++ src/versuchung/experiment.py
@@ -1,3 +1,18 @@
+# This file is part of versuchung.
+# 
+# versuchung is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# versuchung is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# versuchung.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from optparse import OptionParser
 import datetime
 import logging
