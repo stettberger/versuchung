737ed6315f70bdd98cbad99357c5953aad7d504f
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2017-10-17 16:14:06 +0200

    Release 1.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ cmdclass["test"] = TestCommand
 
 version_info = {
     'name': 'versuchung',
-    'version': '1.1',
+    'version': '1.2',
     'description': 'A toolbox for experiments',
     'author': 'Christian Dietrich',
     'author_email': 'stettberger@dokucode.de',
@@ -43,7 +43,7 @@ version_info = {
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.4' ],
+        'Programming Language :: Python :: 2.6' ],
 }
 
 
