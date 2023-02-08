c156c28558e41104e4d4edff844419e7efb5a22a
Marcel Köppen <marcel.koeppen@uos.de>
2023-02-08 16:47:37 +0100

    Advice: Update internal state on disable()

--- tests/advices/test.py
+++ tests/advices/test.py
@@ -12,6 +12,10 @@ class SimpleExperiment(Experiment):
         shell.track(self.path)
         shell("echo 1")
         shell("cd %s && test -x ./sh", "/bin")
+        shell.track.disable()
+        shell("echo I am silent")
+        shell.track.enable()
+        shell("echo Can you hear me?")
 
 if __name__ == "__main__":
     import shutil, sys
@@ -21,7 +25,12 @@ if __name__ == "__main__":
     assert os.path.exists(dirname + "/shell_0_time")
     assert os.path.exists(dirname + "/shell_0_stdout")
     assert os.path.exists(dirname + "/shell_0_stderr")
+    assert os.path.exists(dirname + "/shell_1_stdout")
+    assert os.path.exists(dirname + "/shell_2_stdout")
+    assert not os.path.exists(dirname + "/shell_3_stdout")
 
+    with open(dirname + "/shell_2_stdout") as fd:
+        assert fd.read() == "Can you hear me?\n"
 
     if dirname:
         shutil.rmtree(dirname)
